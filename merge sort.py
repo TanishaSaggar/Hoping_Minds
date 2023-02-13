d8373d33c7cf46026f2f5c33ddeef4ab9145a850
Tanisha Saggar <78544432+TanishaSaggar@users.noreply.github.com>
2023-02-13 16:30:10 +0530

    Add files via upload

---
+++ merge sort.py
@@ -0,0 +1,29 @@
+#merge sort 
+
+def merge(left,right):
+    result=[]
+    i,j=0,0
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            result.append(left[i])
+            i=i+1
+        else:
+            result.append(right[j])
+            j=j+1
+            
+    result+=left[i:]
+    result+=right[j:]
+    return result 
+
+def mergesort(lst):
+    if len(lst)<=1:
+        return lst
+    mid=int(len(lst)/2)
+    left=mergesort(lst[:mid])
+    right=mergesort(lst[mid:])
+    return merge(left,right)
+
+arr=[-1,0,1,2,65,-2,8,-5,11,45,3]
+print(mergesort(arr))
+
+
